28bae47eb0437e1a0c9092d7ce2ccfd84b62f539
naronakshay <235939@ust-global.com>
2022-10-12 17:48:12 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstpageComponent } from './myapp/firstpage/firstpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstpageComponent
   ],
   imports: [
     BrowserModule,
